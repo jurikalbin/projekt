8c5e349f8d21cb79aee784f1030668e343baaccf
Jüri Kalbin <jkalbin@enos.itcollege.ee>
2017-05-30 12:19:08 +0300

    Final commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Saunaleht
 
+Koduleht on loodud I244 koduse projekti eritamiseks.
+Kujundusele ei ole rõhku pannud, aine eesmärk on pigem osata kasutada HTML, PHP, SQL, CSS.
+
+Kodulehel on võimalik vaadata kaardilt asukohta, lisaks siis väike galerii(ei ole piltide lisamise võimalust), võimalik küsida vormi kasutades
+pakkumist ning tellimuste vaatamiseks kodulehele sisse logida. Lisaks eelevale on võimalik sisselogides teha uusi kasutajaid kes saavad vaadata 
+tellimusi. Andmebaasis on olemas ka "roll" aga seda ei ole selles lehenduses kasutusse võetud.
+
+Kodulehele saab sisse loga kasutades:
 u: user
 p: user
--- controller.php
+++ controller.php
@@ -7,16 +7,6 @@ if (isset($_GET['page']) && $_GET['page']!=""){
   	$page=htmlspecialchars($_GET['page']);
 }
 
-	$pildid=array(
-	  array("big"=>"big/soome_saun.jpg", "small"=>"small/soome_saun.jpg", "alt"=>"Soome saun"),
-	  array("big"=>"big/vene_saun.jpg", "small"=>"small/vene_saun.jpg", "alt"=>"Vene Saun"),
-	  array("big"=>"big/turgi_saun.jpg", "small"=>"small/turgi_saun.jpg", "alt"=>"Türgi saun"),
-	  array("big"=>"big/suitsusaun.jpg", "small"=>"small/suitsusaun.jpg", "alt"=>"Suitsusaun"),
-	  array("big"=>"big/aurusaun.jpg", "small"=>"small/aurusaun.jpg", "alt"=>"Aurusaun"),
-	  array("big"=>"big/aroomisaun.jpg", "small"=>"small/aroomisaun.jpg", "alt"=>"Aroomisaun"),
-	);
-
-
 include_once("view/head.html");
 switch($page){
 	case "login":
